6fd5d1b47b60076c3c4a0fc7875a16784833ddb1
KotaroSugawara <KoSugawara@mx1.wiseman.co.jp>
2017-07-25 09:34:41 +0900

    Added entry

---
+++ functions/second/handler.js
@@ -0,0 +1,4 @@
+'use strict';
+
+import 'aws-sdk';
+export { hello } from './hello';
---
+++ functions/second/hello.js
@@ -0,0 +1,18 @@
+export const hello = async (event, context, callback) => {
+  const response = new Promise((resolve, reject) => resolve({
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+      'Access-Control-Allow-Credentials': true // Required for cookies, authorization headers with HTTPS
+    },
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  }));
+
+  callback(null, await response);
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
