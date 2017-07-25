5916c41003599a648cc710f7ca20cfa2ab5c64a1
KotaroSugawara <KoSugawara@mx1.wiseman.co.jp>
2017-07-25 09:33:31 +0900

    Fixed cors

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,10 @@
 export const hello = async (event, context, callback) => {
   const response = new Promise((resolve, reject) => resolve({
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+      'Access-Control-Allow-Credentials': true // Required for cookies, authorization headers with HTTPS
+    },
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function executed successfully!',
       input: event,
